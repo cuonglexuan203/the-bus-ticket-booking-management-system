0f884a32e4d846716719cd8d387043370ce2fdb1
= <=>
2023-04-13 14:11:31 +0700

    update function

--- sql/procedure_function/AutoID/function.sql
+++ sql/procedure_function/AutoID/function.sql
@@ -341,3 +341,189 @@ end
 go
 
 --
+
+create function [dbo].[func_auto_id_cashreserve]()
+returns char(20)
+as
+begin
+declare @id_no char(20)
+set @id_no = (
+    select max(id_cash_reserve)
+    from CASHRESERVE
+)
+if( @id_no is null)
+    set @id_no = concat('cr_', '0000000000')
+declare @no int
+set @no = right(@id_no, 10) + 1;
+return concat('cr_', @no)
+end
+GO
+
+--
+
+
+create function [dbo].[func_auto_id_privilege]()
+returns char(20)
+as
+begin
+declare @id_no char(20)
+set @id_no = (
+    select max(id_privilege)
+    from PRIVILEGE
+)
+if( @id_no is null)
+    set @id_no = concat('pri_', '0000000000')
+declare @no int
+set @no = right(@id_no, 10) + 1;
+return concat('pri_', @no)
+end
+GO
+
+--
+
+
+create function [dbo].[func_auto_id_policy]()
+returns char(20)
+as
+begin
+declare @id_no char(20)
+set @id_no = (
+    select max(id_policy)
+    from PACKAGEPRICEPOLICY
+)
+if( @id_no is null)
+    set @id_no = concat('pol_', '0000000000')
+declare @no int
+set @no = right(@id_no, 10) + 1;
+return concat('pol_', @no)
+end
+GO
+
+
+--
+
+
+create function [dbo].[func_auto_id_event]()
+returns char(20)
+as
+begin
+declare @id_no char(20)
+set @id_no = (
+    select max(id_event)
+    from EVENT
+)
+if( @id_no is null)
+    set @id_no = concat('eve_', '0000000000')
+declare @no int
+set @no = right(@id_no, 10) + 1;
+return concat('eve_', @no)
+end
+GO
+
+
+--
+
+
+create function [dbo].[func_auto_id_package]()
+returns char(20)
+as
+begin
+declare @id_no char(20)
+set @id_no = (
+    select max(id_package)
+    from PACKAGE
+)
+if( @id_no is null)
+    set @id_no = concat('pac_', '0000000000')
+declare @no int
+set @no = right(@id_no, 10) + 1;
+return concat('pac_', @no)
+end
+GO
+
+
+--
+
+
+create function [dbo].[func_auto_id_account]()
+returns char(20)
+as
+begin
+declare @id_no char(20)
+set @id_no = (
+    select max(id_account)
+    from SYSTEMACCOUNT
+)
+if( @id_no is null)
+    set @id_no = concat('acc_', '0000000000')
+declare @no int
+set @no = right(@id_no, 10) + 1;
+return concat('acc_', @no)
+end
+GO
+
+
+--
+
+
+create function [dbo].[func_auto_id_position]()
+returns char(20)
+as
+begin
+declare @id_no char(20)
+set @id_no = (
+    select max(id_position)
+    from POSITION
+)
+if( @id_no is null)
+    set @id_no = concat('pos_', '0000000000')
+declare @no int
+set @no = right(@id_no, 10) + 1;
+return concat('pos_', @no)
+end
+GO
+
+--
+
+
+create function [dbo].[func_auto_id_trip]()
+returns char(20)
+as
+begin
+declare @id_no char(20)
+set @id_no = (
+    select max(id_trip)
+    from TRIP
+)
+if( @id_no is null)
+    set @id_no = concat('tri_', '0000000000')
+declare @no int
+set @no = right(@id_no, 10) + 1;
+return concat('tri_', @no)
+end
+GO
+
+
+--
+
+create function [dbo].[func_auto_id_bus]()
+returns char(20)
+as
+begin
+declare @id_no char(20)
+set @id_no = (
+    select max(id_bus)
+    from BUS
+)
+if( @id_no is null)
+    set @id_no = concat('bus_', '0000000000')
+declare @no int
+set @no = right(@id_no, 10) + 1;
+return concat('bus_', @no)
+end
+GO
+
+
+--
+
+
