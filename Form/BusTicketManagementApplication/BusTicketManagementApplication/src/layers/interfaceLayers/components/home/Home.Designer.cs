ba3cdfbd36c9aca5dc384345a18141a909047ac8
cuonglexuan203 <cuonglexuan203@gmail.com>
2023-04-18 15:49:02 +0700

    update UI

--- Form/BusTicketManagementApplication/BusTicketManagementApplication/src/layers/interfaceLayers/components/home/Home.Designer.cs
+++ Form/BusTicketManagementApplication/BusTicketManagementApplication/src/layers/interfaceLayers/components/home/Home.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(965, 592);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Home";
             this.Text = "Home";
