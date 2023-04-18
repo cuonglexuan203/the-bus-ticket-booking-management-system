ed64f197c0f3d777184f0e790fbd7ae2cfe9e89a
cuonglexuan203 <cuonglexuan203@gmail.com>
2023-04-18 15:53:16 +0700

    update UI

--- Form/BusTicketManagementApplication/BusTicketManagementApplication/src/layers/interfaceLayers/components/home/Home.Designer.cs
+++ Form/BusTicketManagementApplication/BusTicketManagementApplication/src/layers/interfaceLayers/components/home/Home.Designer.cs
@@ -32,11 +32,15 @@
             // 
             // Home
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(965, 592);
+            this.ClientSize = new System.Drawing.Size(1327, 777);
+            this.Font = new System.Drawing.Font("Hack NF", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Home";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Home";
             this.ResumeLayout(false);
 
