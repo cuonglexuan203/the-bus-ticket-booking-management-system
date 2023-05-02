d4e71e6beb594af8a9ceed2259dcc90729e061b8
= <=>
2023-05-02 22:38:21 +0700

    update BS driver, bustation

--- Form/BusTicketManagementApplication/BusTicketManagementApplication/src/layers/businessLayers/BSBusStation.cs
+++ Form/BusTicketManagementApplication/BusTicketManagementApplication/src/layers/businessLayers/BSBusStation.cs
@@ -10,11 +10,11 @@ namespace BusTicketManagementApplication.src.layers.businessLayers
     internal class BSBusStation
     {
 
-        public List<BUSSTATION> GetAllStations()
+        public List<V_BUSSTATIONINFOR> GetAllStations()
         {
             BusManagementEntities db = new BusManagementEntities();
 
-            var res = db.BUSSTATIONs.ToList();
+            var res = db.V_BUSSTATIONINFOR.ToList();
             return res;
         }
     }
--- Form/BusTicketManagementApplication/BusTicketManagementApplication/src/layers/businessLayers/BSDriver.cs
+++ Form/BusTicketManagementApplication/BusTicketManagementApplication/src/layers/businessLayers/BSDriver.cs
@@ -9,32 +9,32 @@ namespace BusTicketManagementApplication.src.layers.businessLayers
 {
    internal class BSDriver
     {
-        public List<DRIVER> GetAllDrivers()
+        public List<V_DRIVERINFOR> GetAllDrivers()
         {
 
             BusManagementEntities db = new BusManagementEntities();
-            var res = db.DRIVERs.ToList();
+            var res = db.V_DRIVERINFOR.ToList();
             return res;
         }
 
 
-        public List<DRIVER> SearchDriver(string input, int tag)
+        public List<V_DRIVERINFOR> SearchDriver(string input, int tag)
         {
             BusManagementEntities db = new BusManagementEntities();
 
-            var res = db.DRIVERs.ToList();
+            var res = db.V_DRIVERINFOR.ToList();
             if (!string.IsNullOrEmpty(input))
             {
                 if (tag == 0)
                 {
-                    res = res.Where(d => d.id_driver.Contains(input.Trim())).ToList();
+                    res = res.Where(d => d.Employees_ID.Contains(input.Trim())).ToList();
                 }
                 else if (tag == 1)
                 {
-                    res = res.Where(d => d.lisence_level.Contains(input.Trim())).ToList();
+                    res = res.Where(d => d.Lisence_Level.Contains(input.Trim())).ToList();
                 }
             }
-            return res.ToList();
+            return res;
         }
     }
 }
